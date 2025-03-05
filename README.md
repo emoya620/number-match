b11e4b9844bedda8e524215dbcd60eda19202599
Emilio Moya <emiliomoya620@gmail.com>
2025-03-05 18:38:55 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# number-match
+ A simple game where you need to quickly identify the number of objects in an image
---
+++ script.js
@@ -0,0 +1,12 @@
+const images = [
+    {path: "images/gameImages/image1", answer: 8},
+    {path: "images/gameImages/image", answer: 0},
+    {path: "images/gameImages/image", answer: 0},
+    {path: "images/gameImages/image", answer: 0},
+    {path: "images/gameImages/image", answer: 0},
+    {path: "images/gameImages/image", answer: 0},
+    {path: "images/gameImages/image", answer: 0},
+    {path: "images/gameImages/image", answer: 0},
+    {path: "images/gameImages/image", answer: 0},
+    {path: "images/gameImages/image", answer: 0}
+]
