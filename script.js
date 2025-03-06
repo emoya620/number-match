1b7f20df0b46e46f7056fdac6927420f06cc3aee
Emilio Moya <emiliomoya620@gmail.com>
2025-03-05 23:25:24 -0500

    Added images to "gameImages" folder

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const images = [
-    {path: "images/gameImages/image1", type: "bell peppers", answer: 8},
-    {path: "images/gameImages/image", type: "", answer: 0},
-    {path: "images/gameImages/image", type: "", answer: 0},
-    {path: "images/gameImages/image", type: "", answer: 0},
-    {path: "images/gameImages/image", type: "", answer: 0},
-    {path: "images/gameImages/image", type: "", answer: 0},
-    {path: "images/gameImages/image", type: "", answer: 0},
-    {path: "images/gameImages/image", type: "", answer: 0},
-    {path: "images/gameImages/image", type: "", answer: 0},
-    {path: "images/gameImages/image", type: "", answer: 0}
+    {path: "images/gameImages/image1.jpg", type: "bell peppers", answer: 8},
+    {path: "images/gameImages/image2.jpg", type: "zebras", answer: 3},
+    {path: "images/gameImages/image3.jpg", type: "candles", answer: 6},
+    {path: "images/gameImages/image4.jpg", type: "donuts", answer: 9},
+    {path: "images/gameImages/image5.jpg", type: "carrots", answer: 7},
+    {path: "images/gameImages/image6.jpg", type: "chickens", answer: 4},
+    {path: "images/gameImages/image7.jpg", type: "ice cream cones", answer: 8},
+    {path: "images/gameImages/image8.jpg", type: "polar bears", answer: 3},
+    {path: "images/gameImages/image9.jpg", type: "pencils", answer: 12},
+    {path: "images/gameImages/image10.jpg", type: "bananas", answer: 6}
 ]
