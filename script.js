c16f29b1d8089b25522bd3b6a9a2097eaf71dd2e
Emilio Moya <emiliomoya620@gmail.com>
2025-03-05 22:34:32 -0500

    Minor refactoring

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const images = [
-    {path: "images/gameImages/image1", answer: 8},
-    {path: "images/gameImages/image", answer: 0},
-    {path: "images/gameImages/image", answer: 0},
-    {path: "images/gameImages/image", answer: 0},
-    {path: "images/gameImages/image", answer: 0},
-    {path: "images/gameImages/image", answer: 0},
-    {path: "images/gameImages/image", answer: 0},
-    {path: "images/gameImages/image", answer: 0},
-    {path: "images/gameImages/image", answer: 0},
-    {path: "images/gameImages/image", answer: 0}
+    {path: "images/gameImages/image1", type: "bell peppers", answer: 8},
+    {path: "images/gameImages/image", type: "", answer: 0},
+    {path: "images/gameImages/image", type: "", answer: 0},
+    {path: "images/gameImages/image", type: "", answer: 0},
+    {path: "images/gameImages/image", type: "", answer: 0},
+    {path: "images/gameImages/image", type: "", answer: 0},
+    {path: "images/gameImages/image", type: "", answer: 0},
+    {path: "images/gameImages/image", type: "", answer: 0},
+    {path: "images/gameImages/image", type: "", answer: 0},
+    {path: "images/gameImages/image", type: "", answer: 0}
 ]
